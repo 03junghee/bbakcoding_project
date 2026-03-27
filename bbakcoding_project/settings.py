7b514c5afb80ec2fc8777727114ce1dfcf6ebb54
03-junghee <03_junghee@naver.com>
2026-03-28 03:40:18 +0900

    Fix: static settings for Vercel deployment

--- bbakcoding_project/settings.py
+++ bbakcoding_project/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'study', 'static'),
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # 배포용 static 모음 폴더
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
